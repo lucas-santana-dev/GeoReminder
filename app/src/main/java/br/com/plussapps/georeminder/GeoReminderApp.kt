25f24ba2b10efd1c9d19b15e29b0a31482a0a6b4
Lucas Santana <lucassantanaads@gmail.com>
2025-08-18 12:27:11 -0300

    feat: inicialização do koin

--- app/src/main/java/br/com/plussapps/georeminder/GeoReminderApp.kt
+++ app/src/main/java/br/com/plussapps/georeminder/GeoReminderApp.kt
@@ -3,12 +3,17 @@ package br.com.plussapps.georeminder
 import android.app.Application
 import br.com.plussapps.georeminder.di.appModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class GeoReminderApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
         startKoin {
+            // Em produção, use Level.NONE; durante dev, ERROR/INFO ajuda
+            androidLogger(Level.ERROR)
             androidContext(this@GeoReminderApp)
             modules(appModule)
         }
